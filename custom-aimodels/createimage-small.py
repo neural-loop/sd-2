0feb2d4417cd557a81150430df931c6796b7a730
Justin Riddiough <jriddiough@gmail.com>
2022-12-08 10:50:35 -0500

    setting up

--- custom-aimodels/createimage-small.py
+++ custom-aimodels/createimage-small.py
@@ -7,6 +7,8 @@ script_args = '--steps 50 '\
   ' --from-file custom-aimodels/prompts.txt '\
   ' --n_samples 1 '\
   ' --n_iter 1 '\
+  ' --H 512 '\
+  ' --W 896 '\
  f' --ckpt /var/meadowrun/machine_cache/{model_name} '\
   ' --config configs/stable-diffusion/v2-inference-v.yaml'\
   ' --outdir /tmp/outputs '
