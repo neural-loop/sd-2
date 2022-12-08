7882635382c50418ceae68e6c09ff2ee1d7482e0
Justin Riddiough <jriddiough@gmail.com>
2022-12-08 10:25:17 -0500

    setting up

--- custom-aimodels/createimage-small.py
+++ custom-aimodels/createimage-small.py
@@ -3,11 +3,10 @@ import sys
 
 model_name = sys.argv[1]
 
-script_args = ' --steps 50 '\
-  ' --from-file \'custom-aimodels/prompts.txt\' '\
- f' --ckpt \'/var/meadowrun/machine_cache/{model_name}\' '\
-  ' --seed 1330 '\
-  ' --config \'configs/stable-diffusion/v2-inference-v.yaml\''\
+script_args = '--steps 50 '\
+  ' --prompt "a professional photograph of an astronaut riding a triceratops" '\
+ f' --ckpt /var/meadowrun/machine_cache/{model_name} '\
+  ' --config configs/stable-diffusion/v2-inference-v.yaml'\
   ' --outdir /tmp/outputs '
 
 subprocess.check_output('python scripts/txt2img.py ' + script_args, shell=True).decode('utf-8')
